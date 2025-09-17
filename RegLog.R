8e8a408184710d5530ecb91e7a85a0c37981135c
Anthony2210 <151615234+Anthony2210@users.noreply.github.com>
2025-09-17 18:26:50 +0200

    update

--- RegLog.R
+++ RegLog.R
@@ -8,8 +8,8 @@ x=read.table("C:/Users/antoc/OneDrive/Bureau/m-1-miashs-1-2-journee-data-science
 
 x <- mutate(x, DIFF = as.factor(DIFF))
 
-# Création des indices pour 70% apprentissage et 30% test
-train_index <- createDataPartition(x$DIFF, p = 0.7, list = FALSE)
+# Création des indices pour 75% apprentissage et 25% test
+train_index <- createDataPartition(x$DIFF, p = 0.75, list = FALSE)
 
 # Jeu d'apprentissage
 train_data <- x[train_index, ]
@@ -27,7 +27,8 @@ modele <- glm(DIFF ~ R2 + R7 + R8 + R17 + R22 + R32,
 
 summary(modele)
 
-# Test des varibales discriminantes
+# TEST de modèles :
+# Test des variables discriminantes (trouvé graphiquement via boxplot)
 
 modele_reduit <- glm(DIFF ~ R2 + R7 + R32,
                      data = train_data, family = binomial)
@@ -38,11 +39,40 @@ AIC(modele, modele_reduit)
 # discriminantes, R2, R7, R32
 # Le modele complet est donc préféré et gardé
 
-# Probabilités prédites sur le jeu de test
-proba_test <- predict(modele, newdata = test_data, type = "response")
+# Test automatique : 
 
-# Classes prédites avec seuil 0.5
-classe_pred_test <- ifelse(proba_test > 0.5, 1, 0)
+modele_step <- step(modele, direction = "both")
+summary(modele_step)
+
+# AIC de ce modele est inferieur à celui du modele complet
+# Ce modele est donc préféré et gardé
+
+modele_inter <- glm(DIFF ~ R2*R32 + R17, data = train_data, family = binomial)
+AIC(modele_step, modele_inter)
+summary(modele_inter)
+
+# AIC de ce modele est inferieur à celui du modele par step
+# Ce modele est donc préféré et gardé
+
+modele_poly <- glm(DIFF ~ poly(R2,2) + poly(R32,2) + R17,
+                   data = train_data, family = binomial)
+AIC(modele_inter, modele_poly)
+
+# AIC de ce modele est supérieur à celui du modele précédent
+# Ce modele n'est donc pas gardé
+
+# Probabilités prédites sur le jeu de test
+proba_test <- predict(modele_inter, newdata = test_data, type = "response")
+
+# Calcul de l'AUC
+roc_obj_test <- roc(test_data$DIFF, proba_test)
+
+# Calcul du seuil optimal
+seuil_opt <- coords(roc_obj_test, "best", ret = "threshold")
+seuil_opt <- as.numeric(seuil_opt)
+
+# Classes prédites avec seuil 
+classe_pred_test <- ifelse(proba_test > seuil_opt, 1, 0)
 
 # Matrice de confusion
 confusionMatrix(
@@ -50,9 +80,7 @@ confusionMatrix(
   factor(test_data$DIFF,   levels = c(0,1))
 )
 
-# Calcul de l'AUC
-roc_obj_test <- roc(test_data$DIFF, proba_test)
-auc(roc_obj)
+auc(roc_obj_test)
 
 # Courbe ROC
 plot(roc_obj_test, col = "blue", lwd = 2, main = "ROC sur données de test")
@@ -64,15 +92,14 @@ data_test=read.table("C:/Users/antoc/OneDrive/Bureau/m-1-miashs-1-2-journee-data
                               sep = ",", dec = ".", header = T)
 
 # Prédictions avec le modèle entraîné sur le 70% train
-proba_test_final <- predict(modele, newdata = data_test, type = "response")
+proba_test_final <- predict(modele_inter, newdata = data_test, type = "response")
 
-classe_pred_test_final <- ifelse(proba_test_final > 0.5, 1, 0)
+classe_pred_test_final <- ifelse(proba_test_final > seuil_opt, 1, 0)
 
-resultats <- data.frame(data_test, 
-                        DIFF = classe_pred_test_final)
+resultats <- data.frame(
+  ID   = 1:nrow(data_test),
+  DIFF = classe_pred_test_final
+)
 
 head(resultats)
 write.csv(resultats, "C:/Users/antoc/OneDrive/Bureau/m-1-miashs-1-2-journee-data-science/predictions_farms_test.csv", row.names = FALSE)
-
-
-
